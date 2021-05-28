e99fe78825a8a78f204545d9764013eee0f2e9c5
white <294476387@qq.com>
2021-05-28 16:47:23 +0800

    整理代码

--- start/src/main/java/com/github/whitechen233/at/AutomatedTestApplication.java
+++ start/src/main/java/com/github/whitechen233/at/AutomatedTestApplication.java
@@ -3,7 +3,6 @@ package com.github.whitechen233.at;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AutomatedTestApplication {
