eead343ca065304d341e2cc5c7d8e05452c7dd34
white <294476387@qq.com>
2021-05-31 18:17:13 +0800

    增加spring security controller

--- start/src/main/java/com/github/whitechen233/at/config/SecurityConfig.java
+++ start/src/main/java/com/github/whitechen233/at/config/SecurityConfig.java
@@ -14,19 +14,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
             // TODO 暂时先使用内存中的 inMemoryAuthentication  需要修改成 userDetailsService
             .inMemoryAuthentication()
+            /*
+             * 在 spring security 5 提供了这样一个思路，应该将密码编码之后的 hash 值和加密方式一起存储，
+             * 并提供了一个 DelegatingPasswordEncoder 来作为众多密码密码编码方式的集合。
+             */
             .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder())
-            .withUser("admin").password("admin").roles("ADMIN")
+            .withUser("admin").password("{noop}admin").roles("ADMIN")
             .and()
-            .withUser("normal").password("normal").roles("NORMAL");
+            .withUser("normal").password("{noop}normal").roles("NORMAL");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("test/echo").permitAll()
-            .antMatchers("test/admin").hasRole("ADMIN")
-            .antMatchers("test/normal").access("hasRole('ROlE_NORMAL')")
+            .antMatchers("/security/echo").permitAll()
+            .antMatchers("/security/admin").hasRole("ADMIN")
+            .antMatchers("/security/normal").access("hasRole('NORMAL')")
             .anyRequest().authenticated()
             .and()
             .formLogin()
